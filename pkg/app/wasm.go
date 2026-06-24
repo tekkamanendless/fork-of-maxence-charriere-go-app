75cd66064ecd17cf399e1f1ba92019393e6c3d31
Douglas Danger Manley <doug.manley@gmail.com>
2026-06-24 02:23:05 -0400

    Add WASM

--- pkg/app/wasm.go
+++ pkg/app/wasm.go
@@ -4,12 +4,18 @@
 package app
 
 import (
+	"net/http"
 	"runtime"
 
 	"github.com/maxence-charriere/go-app/v11/pkg/errors"
 )
 
 func GenerateStaticWebsite(dir string, h *Handler, pages ...string) error {
+	panic(errors.New("unsupported instruction").
+		WithTag("architecture", runtime.GOARCH))
+}
+
+func GenerateStaticWebsiteFromMux(dir string, h http.Handler, pages ...string) error {
 	panic(errors.New("unsupported instruction").
 		WithTag("architecture", runtime.GOARCH))
 }
